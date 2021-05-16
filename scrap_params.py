dfee033549eca23a64d028e17bee1d4707422831
alexvern <alexvernua@gmail.com>
2021-05-16 11:10:42 +0300

    initial

---
+++ scrap_params.py
@@ -0,0 +1,3 @@
+credentials = ['johndoeua', 'Qwer!234']
+boats_list = 'C:/Users/lexx/PycharmProjects/WebScrapper/boatslist.txt'
+scrap_results = 'C:/Users/lexx/PycharmProjects/WebScrapper/boatsdatabase.txt'
